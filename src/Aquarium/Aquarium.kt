bba6e3aba231acf1d225268185b969c065356b40
Tahia <37906654+ootahiaoo@users.noreply.github.com>
2019-03-15 14:21:52 +0900

    add the exercice files

---
+++ src/Aquarium/Aquarium.kt
@@ -0,0 +1,13 @@
+package Aquarium
+
+class Aquarium {
+
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+
+
+    var volume : Int
+        get()= width * height * length / 1000
+        set(value){}
+}
---
+++ src/cookie.kt
@@ -0,0 +1,23 @@
+
+fun main(args: Array<String>) {
+    var fortune: String = ""
+    while (!fortune.contains("Stuff again")) {
+        fortune = getFortune(getBirthday())
+        println("\nYour fortune is: $fortune")
+    }
+}
+
+fun getBirthday() : Int{
+    return readLine()?.toInt() ?:1
+}
+
+fun getFortune(birthday : Int) : String{
+    val fortuneMessage = listOf("Stuff", "Stuff again", "Stuff once more")
+    val index = when (birthday){
+        in 1..15 -> 0
+        in 15..30 -> 1
+        else -> 3
+    }
+    return fortuneMessage[index]
+}
+
