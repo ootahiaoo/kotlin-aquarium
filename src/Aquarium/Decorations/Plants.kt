bb52b6392156e2f7abe4aa1cb26951ba2309585f
Tahia <37906654+ootahiaoo@users.noreply.github.com>
2019-03-20 22:08:50 +0900

    practice annotation

---
+++ src/Aquarium/Decorations/Plants.kt
@@ -0,0 +1,34 @@
+package Aquarium.Decorations
+
+import kotlin.reflect.full.findAnnotation
+
+@ImAPlant class Plant{
+    fun trim(){}
+    fun fertilize(){}
+
+    @get:OnGet
+    val isGrowing: Boolean = true
+
+    @set:OnSet
+    var needsFood: Boolean = false
+}
+
+annotation class ImAPlant
+
+@Target(AnnotationTarget.PROPERTY_GETTER)
+annotation class OnGet
+
+@Target(AnnotationTarget.PROPERTY_SETTER)
+annotation class OnSet
+
+fun reflections(){
+    val classObj = Plant::class
+    for (annotation in classObj.annotations){
+        println(annotation.annotationClass.simpleName)
+    }
+
+    val annotated = classObj.findAnnotation<ImAPlant>()
+    annotated?.apply {
+        println("Found a plant annotation!")
+    }
+}
